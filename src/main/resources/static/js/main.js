01ccde698c6435d860910c8311499bfa952cda34
Raaid <raaidsm@gmail.com>
2021-04-29 00:52:20 -0400

    Implement main.js code for sending and handling AJAX request when form is submitted

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,16 @@
+const handleRestControllerResponse = (result, status, xhr) => {
+    $("#num").text(result.num);
+}
+
+const sendToRestController = () => {
+    $.ajax({
+        url: "/th-spring-integration/spring/ApplyMove",
+        contentType: "application/x-www-form-urlencoded",
+        dataType: "json",
+        success: handleRestControllerResponse()
+    })
+}
+
+$(function() {
+    $("#submitButton").on("click", sendToRestController);
+})
