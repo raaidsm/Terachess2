a218590768dc9027e3cf3823c02a4a2ce5c636ad
Raaid <raaidsm@gmail.com>
2021-04-29 01:10:22 -0400

    Implement more debugging code

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,13 +1,18 @@
-const handleRestControllerResponse = (result, status, xhr) => {
-    $("#num").text(result.num);
+const handleRestControllerResponse = (response, status, xhr) => {
+    console.log("handleRestControllerResponse() runs");
+    $("#num").text(response.num);
 };
 
 const sendToRestController = () => {
+    alert("sentToRestController() runs");
     $.ajax({
         url: "/th-spring-integration/spring/ApplyMove",
         contentType: "application/x-www-form-urlencoded",
         dataType: "json",
-        success: handleRestControllerResponse()
+        success: handleRestControllerResponse,
+        error: function(xhr, status, error) {
+            alert("Bruh why does the rest controller request not work");
+        }
     });
 };
 
