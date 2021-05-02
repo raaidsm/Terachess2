3384d73b0fc41f6de29010d424951752ff4981b0
Raaid <raaidsm@gmail.com>
2021-05-01 21:26:04 -0400

    Create squares in main grid

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,12 @@
-//Global constants
-const gridItemTemplate = "<div class='gridItem border border-3 border-dark m-1'></div>";
+//region Global constants
+const gridItemTemplate = "<div class='gridItem'></div>";
 const gridLength = 8;
 const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];   //Please make this array automatic this is horrendous
 let lightSquareColour = "#EEEED2";
 let darkSquareColour = "#769656";
+//endregion
 
+//region AJAX functions
 const sendRequest = () => {
     let num = $("#num").val();
     $.ajax({
@@ -23,19 +25,22 @@ const handleResponse = (response, status, xhr) => {
     $("#num").val(response.num);
     localStorage.setItem("num", response.num);
 };
+//endregion
 
 $(function() {
-    //Initialize page properties
+    //region Initialize page properties
     $("#submitButton").on("click", sendRequest);
     $("#mainGrid").css("grid-template-columns", `repeat(${gridLength}, 1fr)`);
+    //endregion
 
-    //Initialize Localbase database and initialize default entry
+    //region Initialize Localbase database and initialize default entry
     let db = new Localbase("db");
     db.collection("games").add({ id: 0 });
     const savedNum = localStorage.getItem("num");
     if (savedNum !== null) {
         $("#num").val(savedNum);
     }
+    //endregion
 
     //Dynamically insert elements
     let doLightSquare = false;
@@ -49,7 +54,6 @@ $(function() {
             //Flips the colour
             doLightSquare = doLightSquare === false;
             //Test text to make it not empty
-            $gridItem.text("Test");
             $("#mainGrid").append($gridItem);
         }
     }
