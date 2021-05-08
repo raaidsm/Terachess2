53f73e10069b0706729e4caa75ea8f2462484da2
Raaid <raaidsm@gmail.com>
2021-05-08 18:51:49 -0400

    Add hasInitialPawnMove field to Pawn class

--- src/main/java/raaidsm/spring/test/models/pieces/Pawn.java
+++ src/main/java/raaidsm/spring/test/models/pieces/Pawn.java
@@ -5,6 +5,8 @@ import raaidsm.spring.test.models.Piece;
 import java.util.HashMap;
 
 public class Pawn extends Piece {
+    private boolean hasInitialPawnMove = true;
+
     public Pawn() {}
     public Pawn(String name, String colour, String location, HashMap<String, Piece> board) {
         super(name, colour, location, board);
