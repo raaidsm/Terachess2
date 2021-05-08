9b6e277c18f0868b21c411a02ba1d9af10d10d9d
Raaid <raaidsm@gmail.com>
2021-05-08 18:50:04 -0400

    Add canCastle field to King and Rook classes

--- src/main/java/raaidsm/spring/test/models/pieces/King.java
+++ src/main/java/raaidsm/spring/test/models/pieces/King.java
@@ -5,6 +5,8 @@ import raaidsm.spring.test.models.Piece;
 import java.util.HashMap;
 
 public class King extends Piece {
+    private boolean canCastle = true;
+
     public King() {}
     public King(String name, String colour, String location, HashMap<String, Piece> board) {
         super(name, colour, location, board);
--- src/main/java/raaidsm/spring/test/models/pieces/Rook.java
+++ src/main/java/raaidsm/spring/test/models/pieces/Rook.java
@@ -5,6 +5,8 @@ import raaidsm.spring.test.models.Piece;
 import java.util.HashMap;
 
 public class Rook extends Piece {
+    private boolean canCastle = true;
+
     public Rook() {}
     public Rook(String name, String colour, String location, HashMap<String, Piece> board) {
         super(name, colour, location, board);
