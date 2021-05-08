acb87614f1416d546dbc63771b5aacd24f6d745a
Raaid <raaidsm@gmail.com>
2021-05-08 00:41:28 -0400

    Add legal moves parameter to Pieces and board variable in Rest Contoller

--- src/main/java/raaidsm/spring/test/controllers/TerachessRestController.java
+++ src/main/java/raaidsm/spring/test/controllers/TerachessRestController.java
@@ -5,20 +5,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import raaidsm.spring.test.models.LegalMovesForm;
 import raaidsm.spring.test.models.MoveForm;
+import raaidsm.spring.test.models.Piece;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class TerachessRestController {
     private final Logger logger = LoggerFactory.getLogger(TerachessController.class);
+    private HashMap<String, Piece> board = new HashMap<>();
+
+    public TerachessRestController() {}
 
     @PostMapping(value="/ReadFirstPieceSelection", produces="application/json")
-    public LegalMovesForm readFirstPieceSelection(@ModelAttribute MoveForm form) {
+    public List<String> readFirstPieceSelection(@ModelAttribute MoveForm form) {
         //Receive the square name of a piece and return all legal moves for that piece
         logger.trace("readFirstPieceSelection() runs");
 
-        //TODO: For now, returning empty list of legal moves
-        return new LegalMovesForm();
+        //TODO: For now, returning empty list
+        return new ArrayList<>();
     }
 
     @PostMapping(value="/ReadMove")
--- src/main/java/raaidsm/spring/test/models/Piece.java
+++ src/main/java/raaidsm/spring/test/models/Piece.java
@@ -1,13 +1,16 @@
 package raaidsm.spring.test.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Piece {
+public class Piece implements Serializable {
     private String name = "";
     private String colour = "";
     private String location = "";
+    private List<String> legalMoves = new ArrayList<String>();
 
-    private Piece() {}
+    public Piece() {}
 
     public String getName() {
         return name;
@@ -26,5 +29,11 @@ public class Piece {
     }
     public void setLocation(String location) {
         this.location = location;
+    }
+    public List<String> getLegalMoves() {
+        return legalMoves;
+    }
+    public void setLegalMoves(List<String> legalMoves) {
+        this.legalMoves = legalMoves;
     }
 }
