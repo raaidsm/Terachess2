1a797b3562fd7576205c9c22d428efd5f04b7b8b
Raaid <raaidsm@gmail.com>
2021-05-21 15:10:02 -0400

    Implement upLeft method of Pawn class

--- src/main/java/raaidsm/spring/test/models/pieces/Pawn.java
+++ src/main/java/raaidsm/spring/test/models/pieces/Pawn.java
@@ -57,8 +57,19 @@ public class Pawn extends Piece {
     private MoveCalcResultsStruct upLeft() {
         //Take away initial pawn move since a move has been made
         hasInitialPawnMove = false;
-        //TODO: For now, returning default value
-        return null;
+        //This variable inverts "left" and "right" for black pieces
+        int directionByColour = colour == Colour.WHITE ? 1 : -1;
+        String squareName = location.findRelativeByXAndY(-1 * directionByColour, 1);
+        //Guard clause for relative point going off the board
+        if (squareName == null) return new MoveCalcResultsStruct(null, null, false);
+        Piece pieceAtSquare = board.get(squareName).containedPiece;
+        //Guard clause for there being no piece to capture
+        if (pieceAtSquare == null) return new MoveCalcResultsStruct(null, null, false);
+        //Square has something to capture at this square
+        if (pieceAtSquare.getType() == PieceType.KING) {
+            return new MoveCalcResultsStruct((King)pieceAtSquare, AttackType.ONLY_CAPTURE, true);
+        }
+        return new MoveCalcResultsStruct(null, AttackType.ONLY_CAPTURE, true);
     }
     private MoveCalcResultsStruct upRight() {
         //Take away initial pawn move since a move has been made
