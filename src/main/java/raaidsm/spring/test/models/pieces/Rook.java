cfc419275d76614ee100497dfb0cf9a6d8d54ee9
Raaid <raaidsm@gmail.com>
2021-05-21 23:21:16 -0400

    Implement calculateSquarePreviewResults method for Rook class and only declare moveInALine method

--- src/main/java/raaidsm/spring/test/models/pieces/Rook.java
+++ src/main/java/raaidsm/spring/test/models/pieces/Rook.java
@@ -3,6 +3,11 @@ package raaidsm.spring.test.models.pieces;
 import raaidsm.spring.test.models.Piece;
 import raaidsm.spring.test.models.piece_properties.Colour;
 import raaidsm.spring.test.models.piece_properties.PieceType;
+import raaidsm.spring.test.models.utils.Direction;
+import raaidsm.spring.test.models.utils.MoveCalcResultsStruct;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Rook extends Piece {
     private boolean canCastle = true;
@@ -10,5 +15,20 @@ public class Rook extends Piece {
     public Rook() {}
     public Rook(PieceType name, Colour colour, String location) {
         super(name, colour, location);
+    }
+
+    @Override
+    protected List<MoveCalcResultsStruct> calculateSquarePreviewResults() {
+        List<MoveCalcResultsStruct> results = new ArrayList<>();
+        results.addAll(moveInALine(Direction.UP));
+        results.addAll(moveInALine(Direction.DOWN));
+        results.addAll(moveInALine(Direction.LEFT));
+        results.addAll(moveInALine(Direction.RIGHT));
+        //TODO: For now, return default value
+        return results;
+    }
+    private List<MoveCalcResultsStruct> moveInALine(Direction direction) {
+        //TODO: For now, return default value
+        return new ArrayList<>();
     }
 }
