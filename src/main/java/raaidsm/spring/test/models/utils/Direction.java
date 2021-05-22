b348065a4c3333406f616c339f2a39fe32b36233
Raaid <raaidsm@gmail.com>
2021-05-22 16:43:43 -0400

    Add more values to Direction enum and fix methods in it

--- src/main/java/raaidsm/spring/test/models/utils/Direction.java
+++ src/main/java/raaidsm/spring/test/models/utils/Direction.java
@@ -2,28 +2,41 @@ package raaidsm.spring.test.models.utils;
 
 public enum Direction {
     UP(0, 1),
+    UP_RIGHT(1, 1),
     RIGHT(1, 0),
+    DOWN_RIGHT(1, -1),
     DOWN(0, -1),
-    LEFT(-1, 0);
+    DOWN_LEFT(-1, -1),
+    LEFT(-1, 0),
+    UP_LEFT(-1, 1);
 
+    public int defaultX;
+    public int defaultY;
     public int x;
     public int y;
 
     Direction(int x, int y) {
+        defaultX = x;
+        defaultY = y;
         this.x = x;
         this.y = y;
     }
 
     public int getMagnitude() {
-        assert x == 0 || y == 0;
-        return x + y;
+        checkDirectionValid();
+        if (x != 0) return Math.abs(x);
+        else return Math.abs(y);
     }
     public void setMagnitude(int magnitude) {
-        if (x != 0) x = magnitude;
-        if (y != 0) y = magnitude;
+        x = defaultX * magnitude;
+        y = defaultY * magnitude;
+        checkDirectionValid();
     }
     public void resetMagnitude() {
-        if (x != 0) x = 1;
-        if (y != 0) y = 1;
+        x = defaultX;
+        y = defaultY;
+    }
+    private void checkDirectionValid() {
+        assert (x == 0 || y == 0) || Math.abs(x) == Math.abs(y);
     }
 }
