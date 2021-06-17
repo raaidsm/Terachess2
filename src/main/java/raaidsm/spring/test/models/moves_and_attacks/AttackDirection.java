68aed7aed5671c85444ed91c79b9654b9b31b25c
Raaid <raaidsm@gmail.com>
2021-06-17 01:58:04 -0400

    Create AttackDirection enum

---
+++ src/main/java/raaidsm/spring/test/models/moves_and_attacks/AttackDirection.java
@@ -0,0 +1,9 @@
+package raaidsm.spring.test.models.moves_and_attacks;
+
+public enum AttackDirection {
+    VERTICAL,
+    DIAGONAL_ASCENDING,
+    HORIZONTAL,
+    DIAGONAL_DESCENDING,
+    OTHER
+}
