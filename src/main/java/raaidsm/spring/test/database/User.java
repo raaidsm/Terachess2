c7be199bcafe846a348414d57a1031b4ff5c7e4e
Raaid <raaidsm@gmail.com>
2021-07-12 14:11:27 -0400

    Implement User and UserDataRepo classes

--- src/main/java/raaidsm/spring/test/database/User.java
+++ src/main/java/raaidsm/spring/test/database/User.java
@@ -1,4 +1,28 @@
 package raaidsm.spring.test.database;
 
-public class User {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String colourName;
+
+    public User() {}
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getColourName() {
+        return colourName;
+    }
+    public void setColourName(String colourName) {
+        this.colourName = colourName;
+    }
 }
--- src/main/java/raaidsm/spring/test/database/UserDataRepo.java
+++ src/main/java/raaidsm/spring/test/database/UserDataRepo.java
@@ -1,4 +1,6 @@
 package raaidsm.spring.test.database;
 
-public interface UserDataRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDataRepo extends JpaRepository<User, Integer> {
 }
