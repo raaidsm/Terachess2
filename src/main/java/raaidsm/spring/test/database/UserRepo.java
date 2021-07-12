fe5507f608ea3c1f44fbdf7f82c67c97bf043ad0
Raaid <raaidsm@gmail.com>
2021-07-12 14:12:44 -0400

    Rename UserDataRepo to just UserRepo

--- src/main/java/raaidsm/spring/test/database/UserRepo.java
+++ src/main/java/raaidsm/spring/test/database/UserRepo.java
@@ -2,5 +2,5 @@ package raaidsm.spring.test.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDataRepo extends JpaRepository<User, Integer> {
+public interface UserRepo extends JpaRepository<User, Integer> {
 }
